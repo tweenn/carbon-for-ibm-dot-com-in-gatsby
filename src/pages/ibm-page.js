603a61d603e3d6d146395eff3e6e6756ff1a4ad9
Felipe Zuntini <felipezuntini@hotmail.com>
2022-02-19 01:11:56 -0300

    :construction: (carbon packages) adding pictograms react (dunno why)

--- src/pages/ibm-page.js
+++ src/pages/ibm-page.js
@@ -1,11 +1,22 @@
 import * as React from "react";
-import ReactDOM from 'react-dom';
+import { Column, Grid, Row  } from "carbon-components-react"
 import { DotcomShell } from '@carbon/ibmdotcom-react';
 
 const IndexPage = () => {
   return (
     <DotcomShell>
-      <main>Stuff</main>
+      <Grid>
+        <Row>
+          <Column
+            className="nav-column"
+            sm={4}
+            md={8}
+            lg={11}
+          >
+            <main>Stuff</main>
+          </Column>
+        </Row>
+      </Grid>
     </DotcomShell>
   );
 };
